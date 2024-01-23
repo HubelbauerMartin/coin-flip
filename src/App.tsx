335dbc938ef07ceb85b68ee0c4bcacc7bf32ae26
Martin Hubelbauer <joeysykes18@gmail.com>
2024-01-23 11:47:03 +0100

    Add coin flipping logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import heads from '/heads.png';
 import tails from '/tails.png';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [coin, setCoin] = useState<'heads' | 'tails'>();
+  const handleButtonClick = useCallback(() => {
+    setCoin(() => (Math.random() > 0.5 ? 'heads' : 'tails'));
+  }, []);
 
   return (
     <>
       <h1>Flip a coin</h1>
-      <img src={heads} alt="heads" />
-      <img src={tails} alt="tails" />
+      {(!coin || coin === 'heads') && <img src={heads} alt="heads" />}
+      {(!coin || coin === 'tails') && <img src={tails} alt="tails" />}
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+        <button onClick={handleButtonClick}>Flip</button>
       </div>
     </>
   );

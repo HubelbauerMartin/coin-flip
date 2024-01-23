4c1bd9306eb5ee76d26ff164ed39cbac0ef2d052
Martin Hubelbauer <joeysykes18@gmail.com>
2024-01-23 12:14:53 +0100

    Start with heads and make coin clickable

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,18 @@ import heads from '/heads.png';
 import tails from '/tails.png';
 import './App.css';
 
+const srcMap = { heads, tails };
+
 function App() {
-  const [coin, setCoin] = useState<'heads' | 'tails'>();
-  const handleButtonClick = useCallback(() => {
+  const [coin, setCoin] = useState<'heads' | 'tails'>('heads');
+  const handleClick = useCallback(() => {
     setCoin(() => (Math.random() > 0.5 ? 'heads' : 'tails'));
   }, []);
 
   return (
     <>
       <h1>Flip a coin</h1>
-      {(!coin || coin === 'heads') && <img src={heads} alt="heads" />}
-      {(!coin || coin === 'tails') && <img src={tails} alt="tails" />}
-      <div className="card">
-        <button onClick={handleButtonClick}>Flip</button>
-      </div>
+      <img key={Date.now()} onClick={handleClick} src={srcMap[coin]} alt={coin} title={coin} />
     </>
   );
 }

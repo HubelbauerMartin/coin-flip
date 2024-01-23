0917f93764bff6270dc05f8bcc3cee37bb171464
Martin Hubelbauer <joeysykes18@gmail.com>
2024-01-23 11:32:06 +0100

    Add images assets and use them in code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import heads from '/heads.png';
+import tails from '/tails.png';
 import './App.css';
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
   return (
     <>
       <h1>Flip a coin</h1>
+      <img src={heads} alt="heads" />
+      <img src={tails} alt="tails" />
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
       </div>
